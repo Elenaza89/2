0f02c5f3afdc7b9258866c5a4ec274e42493e3ee
Elnaza <helzahggl@gmail.com>
2023-09-08 16:12:54 +0300

    Коммит Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("введите произвольное число");    
+    
+    try
+    {
+        int N = Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine($"Ввели число {N}");
+        return N;    
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Ввели не число!!!");
+        throw;
+    }       
+}
+
+int N =  GetNumber();
+if(N >= 100 )
+{
+    char[] arr = (N.ToString()).ToArray();
+   
+    Console.WriteLine($"Третья цифра {arr[2]}");
+ }
+ else
+ {
+    Console.WriteLine("3-ей цифры нет");
+ }
+ 
+    
+
